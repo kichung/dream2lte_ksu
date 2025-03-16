3e993a2e98e047747af67845fa15ced3456fd912
kichung <kichung@mbuh.com>
2025-03-16 17:42:28 +0000

    KSU patch

--- fs/stat.c
+++ fs/stat.c
@@ -87,12 +87,22 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 }
 EXPORT_SYMBOL(vfs_fstat);
 
+
+#ifdef CONFIG_KSU
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
+
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		int flag)
 {
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
+
+   #ifdef CONFIG_KSU 
+   ksu_handle_stat(&dfd, &filename, &flag);
+   #endif
+
 
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
@@ -294,6 +304,10 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
+#ifdef CONFIG_KSU
+   ksu_handle_stat(&dfd, &filename, &flag);
+#endif
+
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
@@ -435,6 +449,10 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
+
+#ifdef CONFIG_KSU
+   ksu_handle_stat(&dfd, &filename, &flag); /* 32-bit su */
+#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
